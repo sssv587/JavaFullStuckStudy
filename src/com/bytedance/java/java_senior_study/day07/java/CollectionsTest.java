0070234b639cf6e942e988e449bbfaa90e59570b
sssv587 <1092663295@qq.com>
2021-02-10 02:22:51 +0800

    Collections

--- src/com/bytedance/java/java_senior_study/day07/java/CollectionsTest.java
+++ src/com/bytedance/java/java_senior_study/day07/java/CollectionsTest.java
@@ -22,7 +22,7 @@ public class CollectionsTest {
          shuffle(List)：对 List 集合元素进行随机排序
          sort(List)：根据元素的自然顺序对指定 List 集合元素按升序排序
          sort(List，Comparator)：根据指定的Comparator产生的顺序对List集合元素进行排序
-         swap(List，int， int)：将指定 list 集合中的 i 处元素和 j 处元素进行交换
+         swap(List，int i， int j)：将指定 list 集合中的 i 处元素和 j 处元素进行交换
 
     查找、替换
         Object max(Collection)：根据元素的自然顺序，返回给定集合中的最大元素
@@ -30,7 +30,8 @@ public class CollectionsTest {
         Object min(Collection)
         Object min(Collection，Comparator)
         int frequency(Collection，Object)：返回指定集合中指定元素的出现次数
-        void copy(List dest,List src)：将src中的内容复制到dest中 boolean replaceAll(List list，Object oldVal，Object newVal)：使用新值替换List对象的所有旧值
+        void copy(List dest,List src)：将src中的内容复制到dest中
+        boolean replaceAll(List list，Object oldVal，Object newVal)：使用新值替换List对象的所有旧值
      */
     @Test
     public void test2() {
