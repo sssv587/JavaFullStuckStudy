887db0700c28d35c8165fef82b53ab0659af2c6b
sssv587 <1092663295@qq.com>
2021-02-10 22:55:28 +0800

    IOStream

--- src/com/bytedance/java/java_senior_study/day09/java/InputStreamReaderTest.java
+++ src/com/bytedance/java/java_senior_study/day09/java/InputStreamReaderTest.java
@@ -10,7 +10,7 @@ import java.nio.charset.StandardCharsets;
  * @version 1.0
  * @date 2021/1/24 - 22:08
  * @Description 处理流之二：转换流的使用
- * 1.转韩流：属于字符流
+ * 1.转换流：属于字符流
  * InputStreamReader：字节的输入流转换为字符的输入流
  * OutputStreamWriter：将一个字符的输出流转换为字节的输出流
  * <p>
